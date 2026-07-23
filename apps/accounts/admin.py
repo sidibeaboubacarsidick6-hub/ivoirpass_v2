238231689069f42472f18a588d4b14c4611739fa
sidibeaboubacarsidick6-hub <sidibeaboubacarsidick6@gmail.com>
2026-07-23 19:42:25 +0000

    fix: import settings manquant dans accounts/admin.py (plantait la sauvegarde d'un utilisateur)

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 from django.utils.html import format_html
+from django.conf import settings
 from .models import CustomUser, UserAddress
 
 
